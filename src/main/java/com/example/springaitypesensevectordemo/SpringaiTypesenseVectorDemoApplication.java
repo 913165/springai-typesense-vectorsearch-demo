abfaf5e96154de3300c982c0c8ee60788b5fd14d
Pritesh <tinumistry@gmail.com>
2024-09-22 04:22:17 +0530

    first commit

---
+++ src/main/java/com/example/springaitypesensevectordemo/SpringaiTypesenseVectorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaitypesensevectordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiTypesenseVectorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiTypesenseVectorDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springaitypesensevectordemo/SpringaiTypesenseVectorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaitypesensevectordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiTypesenseVectorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
